a15346d84f3a4be5c4ce595a91d1acc919c1e35f
Matthew Brown <matt@ederoyd.co.uk>
2022-08-19 15:35:58 +0200

    working local service

--- services/store_product/src/bin/local_store_product.rs
+++ services/store_product/src/bin/local_store_product.rs
@@ -1,5 +1,5 @@
 use serde_derive::{Deserialize, Serialize};
-use serde_json::json;
+use serde_json::{json, Value};
 use std::io;
 
 use actix_web::{
@@ -8,13 +8,6 @@ use actix_web::{
     App, HttpResponse, HttpServer, Responder,
 };
 
-/// GraphQL endpoint
-// #[route("/", method = "GET", method = "POST")]
-// async fn graphql(st: web::Data<Schema>, data: web::Json<GraphQLRequest>) -> impl Responder {
-//     let response_data = data.execute(&st, &()).await;
-//     HttpResponse::Ok().json(response_data)
-// }
-
 #[derive(Deserialize, Serialize, Debug)]
 struct AppState {
     id: i64,
@@ -22,8 +15,8 @@ struct AppState {
 }
 
 #[route("/{id}", method = "POST")]
-async fn store_product(id: web::Path<String>, body: web::Json<AppState>) -> impl Responder {
-    web::Json(json!({ "id": id.to_owned(), "body": body.description }))
+async fn store_product(id: web::Path<String>, body: web::Json<Value>) -> impl Responder {
+    HttpResponse::Created().json(json!({ "id": id.to_owned(), "body": body }))
 }
 
 #[tokio::main(flavor = "current_thread")]
