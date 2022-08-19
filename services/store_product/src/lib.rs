c3c7fc0b33393ca112b2786c0e6bdfd0b6f77280
Matthew Brown <matt@ederoyd.co.uk>
2022-08-19 16:12:49 +0200

    added app function

--- services/store_product/src/lib.rs
+++ services/store_product/src/lib.rs
@@ -1,14 +1,26 @@
 use core::database::store_database_item;
 use core::error_and_panic;
-use core::types::{Config, Storable};
+use core::types::{Config, Storable, ConfigBuilder, CustomValue};
+use std::env;
 use lambda_http::{tower::BoxError, Request};
 use log::{error, info, LevelFilter, SetLoggerError};
+use serde_json::Value;
 
 pub fn extract_key_from_request(event: Request) -> String {
     match event.uri().path().rsplit_once('/') {
         Some((_prefix, path_name)) => path_name.to_string(),
         None => error_and_panic!("Could not find Path from the environment"),
     }
+}
+
+pub async fn app(value: Value) -> Result<String, BoxError> {
+    let config = ConfigBuilder::new()
+        .table_name(env::var("DATABASE").unwrap())
+        .build()
+        .await;
+
+    let data = CustomValue { key: "1".to_owned(), value };
+    store_handler(&config, data).await
 }
 
 pub async fn store_handler<T: Storable>(config: &Config, data: T) -> Result<String, BoxError> {
