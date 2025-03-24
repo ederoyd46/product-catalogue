29e7771838874a4930c9df854d376c2876b4edeb
Matthew Brown <matt@ederoyd.co.uk>
2025-03-24 08:29:33 +0000

    Updated dependencies

--- libraries/core/src/types/config.rs
+++ libraries/core/src/types/config.rs
@@ -37,13 +37,13 @@ impl ConfigBuilder {
 
         let aws_sdk_config = match self.endpoint_url {
             Some(url) => {
-                aws_config::defaults(BehaviorVersion::v2024_03_28())
+                aws_config::defaults(BehaviorVersion::v2025_01_17())
                     .endpoint_url(url)
                     .load()
                     .await
             }
 
-            None => aws_config::load_defaults(BehaviorVersion::v2024_03_28()).await,
+            None => aws_config::load_defaults(BehaviorVersion::v2025_01_17()).await,
         };
 
         Config {
